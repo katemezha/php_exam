13f3342f73fa7437ade03c92e61244532e58f5b9
katemezha <44982634+katemezha@users.noreply.github.com>
2020-06-25 16:20:50 +0300

    тбб

--- protokol.php
+++ protokol.php
@@ -22,32 +22,8 @@
 	    <th>Средний Балл</th>
    </tr>
    <?php
-   echo'<tr><td>'.$_POST['pole3'].'</td><td>'.$_POST['pole2'].'</td><td>'.$_POST['pole1'].'</td><td>'.$_POST['pole4'].'</td><td>'.$_POST['pole5'].'</td><td>'.$_POST['pole6'].'</td><td>'.date( "d.m.y" ).'</td><td>'.date( "H:i" ).'</td><td>'.$_SERVER['REMOTE_ADDR'].'</td><td>'.($_POST['pole1']+$_POST['pole2']+strlen($_POST['pole3'])+strlen($_POST['pole4'])).'</td><td>'.(($_POST['pole1']+$_POST['pole2']+strlen($_POST['pole3'])+strlen($_POST['pole4']))/9).'</td></tr>';
+   echo'<tr><td>'.$_POST['pole3'].'</td><td>'.$_POST['pole2'].'</td><td>'.$_POST['pole1'].'</td><td>'.$_POST['pole4'].'</td><td>'.$_POST['pole5'].'</td><td>'.$_POST['pole6'].'</td><td>'.date( "d.m.y" ).'</td><td>'.date( "H:i" ).'</td><td>'.$_SERVER['REMOTE_ADDR'].'</td><td>'.($_POST['pole1']+$_POST['pole2']+strlen($_POST['pole3'])+strlen($_POST['pole4'])).'</td><td>'.(($_POST['pole1']+$_POST['pole2']+strlen($_POST['pole3'])+strlen($_POST['pole4']))/6).'</td></tr>';
    ?>
   </table>
-	<div>
-		<?php
-		// Подключаем файл с классами для работы со столбиками
-	require_once('jpgraph_bar.php');
-
-	// Создаём один столбик
-	$bplot = new BarPlot($xdata);
-
-	$bplot->SetLegend('Инвестиции');
-
-	// Создаём второй столбик
-	$bplot2 = new BarPlot($ydata);
-	$bplot2->SetLegend('Прибыль');
-
-	// Объединяем столбики
-	$accbplot = new AccBarPlot(array($bplot, $bplot2));
-	$accbplot->SetColor('darkgray');
-	$accbplot->SetWeight(1);
-
-	// Присоединяем столбики к графику:
-	$graph->Add($accbplot);
-
-		?>
-	</div>
 </body>
 </html>
